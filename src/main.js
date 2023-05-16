a5ef4ef605a0c64cc907a1e3eadb513ca1409a8d
fbiviano <f.biviano@yahoo.com>
2023-05-16 18:20:51 +0200

    acess

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/html'
+import { configureChains, createConfig } from '@wagmi/core'
+import { arbitrum, mainnet, polygon } from '@wagmi/core/chains'
 
-console.log('Welcome to Vite + JS + Webflow!')
+const chains = [arbitrum, mainnet, polygon]
+const projectId = '0ccda2beb51cc8ed47c47b269c1e9c82'
 
-createBadge()
-animateTitle()
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  publicClient
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
+const web3modal = new Web3Modal({ projectId }, ethereumClient)
