4e77044e9a586321ed0fe28bbc3fbd7802b0ff57
fbiviano <f.biviano@yahoo.com>
2023-05-16 19:08:47 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { configureChains, createClient } from '@wagmi/core'
 import { arbitrum, mainnet, polygon } from '@wagmi/core/chains'
-import { Web3Modal } from '@web3modal/html'
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/html'
 
 const chains = [arbitrum, mainnet, polygon]
 const projectId = '0ccda2beb51cc8ed47c47b269c1e9c82'
