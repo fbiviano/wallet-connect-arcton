75423ea17d07bd62c41b1be4b5fbe84afbf5f58b
fbiviano <f.biviano@yahoo.com>
2023-05-16 17:55:09 +0200

    Update vite.config.js
    change output format

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     rollupOptions: {
       input: './src/main.js',
       output: {
-        format: 'umd',
+        format: 'esm',
         entryFileNames: 'main.js',
         esModule: false,
         compact: true,
